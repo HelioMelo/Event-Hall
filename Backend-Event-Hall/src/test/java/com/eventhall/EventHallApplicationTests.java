5bf84171853e5248bfb71db215dc58c6e66a5383
HelioMelo <Helio_melo_@hotmail.com>
2025-09-15 12:50:18 -0300

    Initial commit: Adicionando projeto Backend Event Hall

---
+++ Backend-Event-Hall/src/test/java/com/eventhall/EventHallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventhall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
